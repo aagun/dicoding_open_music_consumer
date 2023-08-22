5f7cb5ab7c682adab3721900c2da1acc7175c454
Agun <37329998+aagun@users.noreply.github.com>
2023-08-22 17:56:37 +0700

    FIX - listener consumer message broker

--- src/consumer.js
+++ src/consumer.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const amqp = require("amqplib");
 const MailSender = require("./MailSender");
 const Listener = require("./Listener");
+const playlistsService = require("./PlaylistsService");
 
 const init = async () => {
   const mailSender = new MailSender();
-  const listener = new Listener(mailSender);
+  const playlistsService = new PlaylistsService();
+  const listener = new Listener(mailSender, playlistsService);
 
   const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
   const channel = await connection.createChannel();
